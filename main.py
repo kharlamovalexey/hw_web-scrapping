439d9a7463fee427a88cb4438a1337cf2ec88942
kharlamovalexey <128462782+kharlamovalexey@users.noreply.github.com>
2023-07-31 09:13:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
 
     headers = fake_headers.Headers(browser='firefox', os='win')
     headers_dict = headers.generate()
-
-    response = requests.get('https://spb.hh.ru/search/vacancy?text=python&area=1&area=2', headers=headers_dict)
+    url = 'https://spb.hh.ru/search/vacancy?text=django+flask&area=1&area=2'
+    response = requests.get(url, headers=headers_dict)
     html_data = response.text
     soup = bs4.BeautifulSoup(html_data, 'lxml')
     
