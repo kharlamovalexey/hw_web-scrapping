eda336ed08b1422305b5e60b0fa8473b0a63ee8c
kharlamovalexey <128462782+kharlamovalexey@users.noreply.github.com>
2023-07-31 08:56:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ if __name__ == '__main__':
     response = requests.get('https://spb.hh.ru/search/vacancy?text=python&area=1&area=2', headers=headers_dict)
     html_data = response.text
     soup = bs4.BeautifulSoup(html_data, 'lxml')
+    
+    # articles_list = bs.find_all(class_=“vacancy-serp-item__layout”)
+
+    # vacancy_list = []
+
+    # for article in articles_list:
+    #     link = article.find(‘a’)[‘href’]
+    #     salary = article.find(‘span’, class_=“bloko-header-section-3”)
+    #     company = article.find(‘a’, class_=‘bloko-link bloko-link_kind-tertiary’).text
+    #     city = article.find(‘div’,{‘data-qa’:‘vacancy-serp__vacancy-address’}).text
     print()
 
